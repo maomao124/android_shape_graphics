24f65008db8d25b2c9653b3cfa73fcf7fa734ff6
mao <1296193245@qq.com>
2022-09-23 19:21:13 +0800

    android_shape_graphics

---
+++ app/src/main/java/mao/android_shape_graphics/MainActivity4.java
@@ -0,0 +1,16 @@
+package mao.android_shape_graphics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity4 extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+    }
+}
